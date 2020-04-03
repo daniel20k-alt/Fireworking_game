e365c90cccc6de664d1a980968300778dc0b60fd
Testing_author <testing_beta@mail.com>
2020-04-03 20:24:35 +0300

    Creating the checkTouches fun

--- Fireworking/GameScene.swift
+++ Fireworking/GameScene.swift
@@ -19,10 +19,10 @@ class GameScene: SKScene {
     
     var score = 0 {
         didSet {
-        //coding later to be added for updating
+            //coding later to be added for updating
+        }
     }
-}
-
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
@@ -51,7 +51,7 @@ class GameScene: SKScene {
         default:
             firework.color = .red
         }
-    
+        
         let path = UIBezierPath()
         path.move(to: .zero)
         path.addLine(to: CGPoint(x: xMovement, y: 1000))
@@ -102,9 +102,43 @@ class GameScene: SKScene {
             createFirework(xMovement: -movementAmount, x: rightEdge, y:bottomEdge + 200)
             createFirework(xMovement: -movementAmount, x: rightEdge, y:bottomEdge + 100)
             createFirework(xMovement: -movementAmount, x: rightEdge, y:bottomEdge)
-        
+            
         default:
             break
         }
     }
+    
+    func checkTouches(_ touches: Set<UITouch>) {
+        guard let touch = touches.first else { return }
+        
+        let location = touch.location(in: self)
+        let nodesAtPoint = nodes(at: location)
+        
+        for case let node as SKSpriteNode in nodesAtPoint {
+            guard node.name == "firework" else { continue }
+            
+            for parent in fireworks {
+                guard let firework = parent.children.first as? SKSpriteNode else { continue }
+                
+                if firework.name == "selected" && firework.color != node.color {
+                    firework.name = "firework"
+                    firework.colorBlendFactor = 1
+                }
+            }
+            
+            node.name = "selected"
+            node.colorBlendFactor = 0
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        checkTouches(touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        checkTouches(touches)
+    }
+    
 }
